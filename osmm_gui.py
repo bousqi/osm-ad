4fc26e8205266f29732a9d2d9871d2848e456874
Bousqi <bousqi@gmail.com>
2022-01-17 22:04:45 +0100

    QtreeWidget populated, first shot

--- osmm_gui.py
+++ osmm_gui.py
@@ -14,7 +14,7 @@ class osmm_gui(QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
 
         # class instance vars init
-        self.index_dict = {}
+        self.indexes = {}
         # UI init
         self.initUI()
 
@@ -23,29 +23,50 @@ class osmm_gui(QMainWindow, Ui_MainWindow):
 
         # overloading app behavior
         self.actionRefresh.triggered.connect(self.UI_updateIndexList)
-        self.refreshButton.clicked.connect(self.UI_updateIndexList)
         self.UI_displayItemCount()
 
     def UI_updateIndexList(self):
-        self.index_dict = osmm_FeedIndex()
+        self.indexes = osmm_ProcessIndexes(osmm_FeedIndex())
 
         # empty the list
         self.osmm_treeWidget.clear()
-        # # adding items
-        # for ossm_item in self.index_dict:
-        #     # to be displayed, creating main entry
-        #     item = QtWidgets.QTreeWidgetItem()
-        #     item.setText(0, self.index_dict[ossm_item])
-        #
-        #     self.osmm_treeWidget.addTopLevelItem(item)
-        #
-        self.osmm_treeWidget.expandAll()
 
+        # adding items
+        categories = osmm_GetCategories(self.indexes)
+        for cat in categories:
+            item = QtWidgets.QTreeWidgetItem()
+            item.setText(0, cat)
+
+            # getting Countries/Areas
+            areas = osmm_GetCountries(self.indexes, cat)
+            for area in areas:
+                item_area = QtWidgets.QTreeWidgetItem(item)
+                item_area.setText(0, area)
+
+                sub_indexes = osmm_FilterIndex(self.indexes, cat, area)
+                for ossm_item in sub_indexes:
+                    sub_item = QtWidgets.QTreeWidgetItem(item_area)
+                    sub_item.setText(2, ossm_item["@name"])
+                    sub_item.setText(3, ossm_item["@date"])
+                    sub_item.setText(4, ossm_item["@size"] + " MB")
+                    sub_item.setTextAlignment(4, QtCore.Qt.AlignRight)
+                    sub_item.setText(5, ossm_item["@targetsize"] + " MB")
+                    sub_item.setTextAlignment(5, QtCore.Qt.AlignRight)
+                    sub_item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+            self.osmm_treeWidget.addTopLevelItem(item)
+
+        self.osmm_treeWidget.setColumnWidth(0, 100)
+        self.osmm_treeWidget.setColumnWidth(1, 150)
+        self.osmm_treeWidget.setColumnWidth(2, 300)
+        self.osmm_treeWidget.setColumnWidth(3, 100)
+        self.osmm_treeWidget.setColumnWidth(4, 100)
+        self.osmm_treeWidget.setColumnWidth(5, 100)
+        # self.osmm_treeWidget.collapseAll()
+
+        self.UI_displayItemCount()
 
 
     def UI_displayItemCount(self):
-        count = 0
-
-        # counting current displayed items in list
-
-        self.statusbar.showMessage("{} - {}/{}     (Disp - Download/Total)".format(count, count, count))
+        self.statusbar.showMessage("{} - {}/{}     (Disp - Download/Total)".format(len(self.indexes),
+                                                                                   len(osmm_GetDownloads(self.indexes)),
+                                                                                   len(self.indexes)))
